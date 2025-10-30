f00f21eba71b7d96a8a85d2af9ff70711812faa7
Leon2353 <lboeckm2@smail.uni-koeln.de>
2025-10-30 16:14:43 +0100

    Praesenzaufgabe 3

---
+++ src/p3/P3_main.java
@@ -0,0 +1,29 @@
+package p3;
+
+public class P3_main {
+
+	public static void main(String[] args) {
+
+		int i = 2;
+		int j = 2;
+		int k = 3;
+
+		if (i < j) {
+
+			System.out.print("A");
+
+		}
+		if (i < k) {
+
+			System.out.print("B");
+
+		}
+		if (j < k) {
+
+			System.out.print("C");
+
+		}
+
+	}
+
+}
